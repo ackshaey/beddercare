1912ebc60a2ed1b110b8dbdcf7da66e2e078a29b
Ackshaey Singh <ackshaey@gmail.com>
2013-11-17 01:44:09 -0800

    changed url for actionmailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "beddercare@gmail.com"
   def notification_email(user,room)
   	@user = user
-  	@room = room
-  	@url = "http://beddercare.herokuapp.com/party/"+@room.id.to_s
-  	mail(to: "ackshaey@gmail.com", subject: 'Welcome to My Awesome Site')
+  	@url = "http://beddercare.herokuapp.com/party/"+room.id.to_s
+  	mail(to: "ackshaey@gmail.com", subject: 'Beddercare : Video Chat Invitation')
   end
 end
