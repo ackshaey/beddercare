91517533b79998ef794e8631c0de48be6eaa62d8
Ackshaey Singh <ackshaey@gmail.com>
2013-11-17 02:08:26 -0800

    Fixed logic for retrieving correct user id from room id for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "beddercare@gmail.com"
-  def notification_email(user,room_id)
-  	@user = user
-  	@url = "http://beddercare.herokuapp.com/party/#{room_id}"
+  def notification_email(new_room)
+  	@new_room = new_room
+  	@user = User.find(@new_room.user_id)
+  	id = @new_room.id.to_s
+  	@url = "http://beddercare.herokuapp.com/party/#{id}"
   	mail(to: @user.email, subject: 'Beddercare : Video Chat Invitation')
   end
 end
